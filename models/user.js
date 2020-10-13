bb366fa40605bcd7fe937e9b1946b33a67d3add1
Calvin Griffin <cgriffin332@gmail.com>
2020-10-13 18:22:27 -0400

    updated sample model to user model

---
+++ models/user.js
@@ -0,0 +1,13 @@
+module.exports = function(sequelize, DataTypes){
+    const User = sequelize.define("User", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+    return User;
+};
