7ecb848859a2851d50cc10663984d3912fe44f63
Calvin Griffin <cgriffin332@gmail.com>
2020-10-13 20:48:49 -0400

    added route to login page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,8 +4,11 @@ const path = require("path");
 
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+  // Each of the below routes just handles the HTML pages
 
-
+  // index route loads login page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/login.handlebars"));
+  });
 
 };
