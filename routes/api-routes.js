e07a1729e027bf1012621aeaa1549243537ca579
Calvin Griffin <cgriffin332@gmail.com>
2020-10-13 22:59:59 -0400

    get user by email

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,4 +9,21 @@ module.exports = function (app) {
         res.json(newUser)
     });
   });
+  // get all users
+  app.get("/api/user", (req, res) =>{
+    db.User.findAll().then((allUsers) =>{
+      res.json(allUsers);
+    });
+  });
+  // get user by email
+  app.get("/api/user/:email", function(req, res) {
+    db.User.findAll({
+      where: {
+        email: req.params.email,
+      }
+    }).then((user) => {
+      res.json(user);
+    });
+  });
+
 };
