af8eec32460e323e460228b15d88ddfba1a3db85
Zach Drummond <zachdrummond3@gmail.com>
2020-10-14 18:59:16 -0400

    Added post route for creating a new user on signup page

--- models/user.js
+++ models/user.js
@@ -1,13 +1,19 @@
 module.exports = function (sequelize, DataTypes) {
-  const User = sequelize.define("User", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const User = sequelize.define(
+    "User",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: false,
+    }
+  );
   return User;
 };
