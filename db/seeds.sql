480b0051c81f13a3ece658d8a5f2a8ec711a3821
diana maina <dianamainachui@gmail.com>
2020-10-13 20:49:04 -0400

    modified schema/seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,4 @@
-INSERT INTO (tName) ((colName)) 
-VALUES ((value));
+
+INSERT INTO sentGifts (users_id, receiverName, receiveraddress, gift, cost , occasion, date)
+VALUE (1 ,'name', 'address', 'goat', 225.50, 'wedding',  03/25/2017);
+select * from sentGifts;
