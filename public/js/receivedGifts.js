7c1edcdc48445a37f6bcfc178b2e5f99e058590c
diana maina <dianamainachui@gmail.com>
2020-10-14 13:53:33 -0400

    added recieved gifts.js

--- public/js/receivedGifts.js
+++ public/js/receivedGifts.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
-    // Getting references to our form and inputs
-    var addGift = $("form.login");
+  // Getting references to our form and input
+  const receivedGifts = $("newGift");
+
+  // When the add gift button is clicked, we validate the  gift is not blank
+  receivedGifts.on("submit", function(event) {
+    event.preventDefault();
+    const giftsData = {
+      giftsInput: giftInput.val().trim(),
+    };
+
+    if (!userData.email || !userData.password) {
+      return;
+    }
+    // If we have a new gift  run the receviedGift function
+    receivedGifts(giftsData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function $(document).ready(function() {
+    // Getting references to our form and input
+    var signUpForm = $("form.signup");
     var emailInput = $("input#email-input");
     var passwordInput = $("input#password-input");
   
-    // When the form is submitted, we validate there's an email and password entered
-    loginForm.on("submit", function(event) {
+    // When the signup button is clicked, we validate the email and password are not blank
+    signUpForm.on("submit", function(event) {
       event.preventDefault();
       var userData = {
         email: emailInput.val().trim(),
@@ -15,25 +37,45 @@ $(document).ready(function() {
       if (!userData.email || !userData.password) {
         return;
       }
-  
-      // If we have an email and password we run the loginUser function and clear the form
-      loginUser(userData.email, userData.password);
+      // If we have an email and password, run the signUpUser function
+      signUpUser(userData.email, userData.password);
       emailInput.val("");
       passwordInput.val("");
-    }); 
+    });
   
-    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-    function loginUser(email, password) {
-      $.post("/api/login", {
+    // Does a post to the signup route. If successful, we are redirected to the members page
+    // Otherwise we log any errors
+    function signUpUser(email, password) {
+      $.post("/api/signup", {
         email: email,
         password: password
       })
-        .then(function() {
+        .then(function(data) {
           window.location.replace("/members");
-          // If there's an error, log the error
+          // If there's an error, handle it by throwing up a bootstrap alert
         })
-        .catch(function(err) {
-          console.log(err);
-        });
+        .catch(handleLoginErr);
+    }
+  
+    function handleLoginErr(err) {
+      $("#alert .msg").text(err.responseJSON);
+      $("#alert").fadeIn(500);
     }
   });
+  (email, password) {
+    $.post("/api/signup", {
+      email: email,
+      password: password
+    })
+      .then(function(data) {
+        window.location.replace("/members");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
