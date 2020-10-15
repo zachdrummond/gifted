77ca597b891f417669ffb780d5c375d69dd4ca44
Calvin Griffin <cgriffin332@gmail.com>
2020-10-15 00:14:38 -0400

    formatted sentGifts pattern

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -21,7 +21,7 @@ module.exports = function (app) {
   app.get("/received", function (req, res) {
     db.ReceivedGifts.findAll()
       .then((allReceivedGifts) => {
-        console.log(allReceivedGifts);
+        // console.log(allReceivedGifts);
         res.render("receivedGifts", { gifts: allReceivedGifts });
       })
       .catch((err) => {
