f89907b2de9858443329ed83ed45bb88cd67c98c
diana maina <dianamainachui@gmail.com>
2020-10-17 17:49:40 -0400

    organised tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,9 @@
 INSERT INTO  users (name, email) VALUES ('Name', 'email@email.com');
 
 INSERT INTO gifts_db.ReceivedGifts (user_id, senderName, senderAddress,  giftReceived, occasion, dateReceived, thankYou) 
-VALUES (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-05-26' ,1), (1, 'David Mann', '44 50th St.' , 'Mario Cart', 'Birthday', '2016-04-17' ,1),(1, 'Sarah Roberts', '33 South Dr.' , 'Socks', 'Birthday', '2012-04-17' ,0), (1, 'Jessica Doon', '45 Welcome Dr.' , 'Chess Set', 'Christmas', '2018-12-25' ,0), 
+VALUES (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-05-26' ,1), (1, 'David Mann', '44 50th St.' , 'Mario Cart', 'Birthday', '2016-04-17' ,1),
+(1, 'Sarah Roberts', '33 South Dr.' , 'Socks', 'Birthday', '2012-04-17' ,0),
+(1, 'Jessica Doon', '45 Welcome Dr.' , 'Chess Set', 'Christmas', '2018-12-25',0), 
 (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-05-26' ,1),
 (1, 'Denise Jones', '27 Apple street.', 'Office chairs', 'Birthday', '2008-08-28',1),
 (1, 'Brian Michaels', '818 Humble Rd.' , 'Dinner Plates', 'Graduation', '2013-12-16',1),
@@ -15,7 +17,10 @@ VALUES (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-
 (1, 'John Stone', '70 Barcelona Crt.' , 'Ski Blades', 'Birthday', '2019-05-26' ,1);
 
 INSERT INTO gifts_db.sentGifts (user_id, receiverName, receiveraddress, giftSent, cost , occasion, dateSent)
-VALUE (1 ,'Harry Herbert', '1212 Peachtree St.', 'Goat', 225.50, 'Wedding', '2017-03-25'), (1 ,'Emily James', '555 Fruit St.', 'Flowers', 30, 'Birthday', '2020-03-25'), (1 ,'Katie Melvin', '90 Jackson St.', 'Dog', 150, 'Christmas', '2013-12-25'), (1 ,'Tobie Tate', '34 Starlight Dr.', 'Bourbon', 42, 'Christmas', '2016-12-25'),
+VALUE (1 ,'Harry Herbert', '1212 Peachtree St.', 'Goat', 225.50, 'Wedding', '2017-03-25'),
+(1 ,'Emily James', '555 Fruit St.', 'Flowers', 30, 'Birthday', '2020-03-25'),
+(1 ,'Katie Melvin', '90 Jackson St.', 'Dog', 150, 'Christmas', '2013-12-25'),
+(1 ,'Tobie Tate', '34 Starlight Dr.', 'Bourbon', 42, 'Christmas', '2016-12-25'),
 (1 ,'Harry Herbert', '1212 Peachtree St.', 'Goat', 225.50, 'Wedding', '2017-03-25'),
 (1 ,'Harry Herbert', '1212 Peachtree St.', 'Goat', 225.50, 'Wedding', '2017-03-25'),
 (1 ,'Harry Herbert', '1212 Peachtree St.', 'Goat', 225.50, 'Wedding', '2017-03-25'),
