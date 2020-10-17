ca1648fea64d58bb7ca7c5dd923f174f818cd818
diana maina <dianamainachui@gmail.com>
2020-10-17 17:15:06 -0400

    added chair

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,7 +3,7 @@ INSERT INTO  users (name, email) VALUES ('Name', 'email@email.com');
 INSERT INTO gifts_db.ReceivedGifts (user_id, senderName, senderAddress,  giftReceived, occasion, dateReceived, thankYou) 
 VALUES (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-05-26' ,1), (1, 'David Mann', '44 50th St.' , 'Mario Cart', 'Birthday', '2016-04-17' ,1),(1, 'Sarah Roberts', '33 South Dr.' , 'Socks', 'Birthday', '2012-04-17' ,0), (1, 'Jessica Doon', '45 Welcome Dr.' , 'Chess Set', 'Christmas', '2018-12-25' ,0), 
 (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-05-26' ,1),
-(1, 'Denise Jones', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-05-26' ,1),
+(1, 'Denise Jones', '27 Apple street.', 'Office chairs', 'Birthday', '2008-08-28',1),
 (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-05-26' ,1),
 (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-05-26' ,1),
 (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-05-26' ,1),
