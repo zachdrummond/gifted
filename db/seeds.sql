ac66f1e3278db6d1b2919131abb9aeefeaa92f04
diana maina <dianamainachui@gmail.com>
2020-10-18 12:33:53 -0400

    modified seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,7 +17,7 @@ VALUES (1, 'Michael James', '122 North Dr.' , 'Golf Balls', 'Graduation', '2007-
 (1, 'John Stone', '70 Barcelona Crt.' , 'Ski Blades', 'Birthday', '2019-05-26' ,1);
 
 INSERT INTO gifts_db.sentGifts (user_id, receiverName, receiveraddress, giftSent, cost , occasion, dateSent)
-VALUE (1 ,'Harry Herbert', '1212 Peachtree St.', 'Goat', 225.50, 'Wedding', '2017-03-25'),
+VALUE (1,'Harry Herbert', '1212 Peachtree St.', 'Goat', 225.50, 'Wedding', '2017-03-25'),
 (1 ,'Emily James', '555 Fruit St.', 'Flowers', 30, 'Birthday', '2020-03-25'),
 (1 ,'Katie Melvin', '90 Jackson St.', 'Dog', 150, 'Christmas', '2013-12-25'),
 (1 ,'Tobie Tate', '34 Starlight Dr.', 'Bourbon', 42, 'Christmas', '2016-12-25'),
